eac5fba31ab35e97ef8622981d991172083821a1
henriksson666 <128688845+henriksson666@users.noreply.github.com>
2023-09-04 16:23:02 -0300

    Update TrainSimulator.java

--- TrainSimulator/src/TrainSimulator.java
+++ TrainSimulator/src/TrainSimulator.java
@@ -1,5 +1,23 @@
-public class TrainSimulator {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.animation.PathTransition;
+import javafx.application.Application;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class TrainSimulator extends Application {
+
+    private boolean isPlaying = false;
+    private boolean isReverse = false;
+    Pane root;
+    int configuration = 1;
+    private PathTransition train1Transition, train3Transition;
+
+    @Override
+    public void start(Stage primaryStage) {
+        
     }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
 }
