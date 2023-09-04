3a36e74a711b70896a43b45139c9f78ff1a29617
henriksson666 <128688845+henriksson666@users.noreply.github.com>
2023-09-04 16:25:10 -0300

    Update TrainSimulator.java

--- TrainSimulator/src/TrainSimulator.java
+++ TrainSimulator/src/TrainSimulator.java
@@ -1,5 +1,6 @@
 import javafx.animation.PathTransition;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -9,11 +10,13 @@ public class TrainSimulator extends Application {
     private boolean isReverse = false;
     Pane root;
     int configuration = 1;
-    private PathTransition train1Transition, train3Transition;
+    private PathTransition train1Transition, train2Transition;
 
     @Override
     public void start(Stage primaryStage) {
-        
+        root = new Pane();
+        Scene scene = new Scene (root, 1300, 690);
+        int[] clickCount = {1};
     }
 
     public static void main(String[] args) throws Exception {
