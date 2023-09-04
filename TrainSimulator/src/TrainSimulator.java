fc914cbd1af4b8df0648da1857543dd8286fb18f
henriksson666 <128688845+henriksson666@users.noreply.github.com>
2023-09-04 16:33:00 -0300

    Update TrainSimulator.java

--- TrainSimulator/src/TrainSimulator.java
+++ TrainSimulator/src/TrainSimulator.java
@@ -1,7 +1,11 @@
+import java.util.Random;
+
 import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
@@ -26,6 +30,27 @@ public class TrainSimulator extends Application {
         Image backgroundImage = new Image("background4.png");
         BackgroundSize backgroundSize = new BackgroundSize(1300, 690, false, false, false, false);
         BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, backgroundSize);
+        root.setBackground(new Background(background));
+
+        int sizeWeight = 70;
+        int sizeHeight = 50;
+
+        ImageView trainImageView1 = new ImageView(createImageTrain());
+        trainImageView1.setFitWidth(sizeWeight);
+        trainImageView1.setFitHeight(sizeHeight);
+
+        ImageView trainImageView2 = new ImageView(createImageTrain());
+        trainImageView2.setFitWidth(sizeWeight);
+        trainImageView2.setFitHeight(sizeHeight);
+        
+    }
+
+    private Image createImageTrain() {
+        Random rand = new Random();
+        int selectedNumber = rand.nextInt(4) + 1;
+        String imageName = "train" + selectedNumber + ".png";
+        
+        return new Image(TrainSimulator.class.getResourceAsStream(imageName));
     }
 
     public static void main(String[] args) throws Exception {
