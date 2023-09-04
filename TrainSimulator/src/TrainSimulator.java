9ebcb31c423928e34f6720740976d9139b8dc0d0
henriksson666 <128688845+henriksson666@users.noreply.github.com>
2023-09-04 16:38:53 -0300

    Update TrainSimulator.java

--- TrainSimulator/src/TrainSimulator.java
+++ TrainSimulator/src/TrainSimulator.java
@@ -11,6 +11,9 @@ import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.stage.Stage;
 
 public class TrainSimulator extends Application {
@@ -42,14 +45,27 @@ public class TrainSimulator extends Application {
         ImageView trainImageView2 = new ImageView(createImageTrain());
         trainImageView2.setFitWidth(sizeWeight);
         trainImageView2.setFitHeight(sizeHeight);
+
+        Path railPath1 = createPath(new double[] { 0, 30, 0, -30, 0, 30, 0, -30, 0 }, 0, 479, 154, 50, false);
+        Path railPath2 = createPath(new double[] { 0, -30, 0, 30, 0, -30, 0, 30, 0 }, 0, 325, 154, 50, false);
         
+    }
+
+    private Path createPath(double[] angles, double x, double y, double length, int numIntermediatePoints, boolean isReverse) {
+        Path path = new Path();
+        path.setStroke(Color.BLUE);
+        path.getElements().add(new MoveTo(x, y));
+
+        if (isReverse) {
+            for ()
+        }
     }
 
     private Image createImageTrain() {
         Random rand = new Random();
         int selectedNumber = rand.nextInt(4) + 1;
         String imageName = "train" + selectedNumber + ".png";
-        
+
         return new Image(TrainSimulator.class.getResourceAsStream(imageName));
     }
 
